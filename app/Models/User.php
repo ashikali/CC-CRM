fb97f73405f25f8f581ae09d5905d602197569d6
Kirat31 <kiratkaurbharaj@gmail.com>
2023-06-14 16:44:06 +0530

    min pass edit3

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,6 @@ use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
